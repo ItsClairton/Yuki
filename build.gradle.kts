e6bb858e21a467d8aaa413acf0858f7c4aa227d3
Clairton Santos <hi@clairton.dev>
2024-09-28 21:19:57 -0300

    chore: Compatibility with >= JDK 17 runtimes

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 group = "ac.grim.grimac"
 version = "2.3.67"
 
-java.sourceCompatibility = JavaVersion.VERSION_21
-java.targetCompatibility = JavaVersion.VERSION_21
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 repositories {
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // Spigot
